3f3d5d8a9c809d4cad840ccdab4a8aa15d9f3e76
Karolina Dudkowska <karoladudkowska@wp.pl>
2020-12-03 19:01:13 +0100

    28-js-warunki

---
+++ js/main5.js
@@ -0,0 +1,9 @@
+let amountOfProducts = 12;
+if (amountOfProducts > 10)
+{
+    console.log('Podana liczba jest większa od 10');
+}
+else
+{
+    console.log('Podana liczba nie jest większa od 10');
+}

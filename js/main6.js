9e8b7ef58aa6b5d25edd46d6e22651e9976e35db
Karolina Dudkowska <karoladudkowska@wp.pl>
2020-12-03 22:56:38 +0100

    29-js-suma-zmiennych

---
+++ js/main6.js
@@ -0,0 +1,18 @@
+let firstNumber = 11;
+let secondNumber = 15;
+
+function adding(firstNumber, secondNumber)
+{
+    let result = firstNumber + secondNumber;
+    
+    
+    return result;
+    
+   
+
+    
+    
+}
+
+
+console.log(adding(firstNumber, secondNumber));

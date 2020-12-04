79d6d5d197dc0b933111c6acd6e4db3ae2cf70bd
Karolina Dudkowska <karoladudkowska@wp.pl>
2020-12-04 19:29:09 +0100

    corection parametres

--- 29-js- suma-zmiennych/main.js
+++ 29-js- suma-zmiennych/main.js
@@ -1,17 +1,9 @@
 let firstNumber = 11;
 let secondNumber = 15;
 
-function adding(firstNumber, secondNumber) {
-    let result = firstNumber + secondNumber;
-
-
+function adding(num1, num2) {
+    let result = num1 + num2;
     return result;
-
-
-
-
-
 }
-
 
 console.log(adding(firstNumber, secondNumber));

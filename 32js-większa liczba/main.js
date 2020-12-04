8d6384b30d3178c6f716643f95b9f98e5ee150b4
Karolina Dudkowska <karoladudkowska@wp.pl>
2020-12-04 17:26:23 +0100

    new names files24-js-konsola

---
+++ 32js-większa liczba/main.js
@@ -0,0 +1,17 @@
+let num1 = 2;
+let num2 = 5;
+
+
+let result = function checkingNumbers() {
+
+    if (num1 > num2) {
+        console.log(num1);
+    }
+
+    else if (num1 < num2) {
+        console.log(num2)
+    }
+}
+
+
+function checkingNumbers(num1, num2);

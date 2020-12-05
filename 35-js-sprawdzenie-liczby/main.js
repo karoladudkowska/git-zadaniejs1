0c72d4f877c0bf794206f87b9358d5dd7407b1e8
Karolina Dudkowska <karoladudkowska@wp.pl>
2020-12-05 18:03:41 +0100

    35-js-sprawdzenie-liczby

---
+++ 35-js-sprawdzenie-liczby/main.js
@@ -0,0 +1,11 @@
+const number = 110;
+
+function verifactionNumbers(n) {
+    if (n > 100 && n < 200) {
+        console.log("Liczba znajduje się w przedziale");
+    }
+    else {
+        console.log("Liczba nie znajduje się w przedziale");
+    }
+}
+verifactionNumbers(number)

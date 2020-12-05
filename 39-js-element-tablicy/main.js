d8c44d83c644cffae043d78d168640095adcfc7b
Karolina Dudkowska <karoladudkowska@wp.pl>
2020-12-05 20:41:11 +0100

    39-js-element-tablicy

---
+++ 39-js-element-tablicy/main.js
@@ -0,0 +1,7 @@
+let numbers = [1, 2, 3, 4, 5];
+function showFirstFromTabel(tabel) {
+    let firstElementOfTabel = tabel[0];
+    console.log(firstElementOfTabel);
+}
+
+showFirstFromTabel(numbers)
